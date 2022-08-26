920b950567e66da9535973747cbf8de15268609b
Santiago-Alejandro <srodriguezcu@unal.edu.co>
2022-08-26 17:28:10 -0500

    primer codigo

---
+++ programa.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// using namespace std; // BAD
+
+
+
+int main(void) {
+    return 0;
+}
+std:cout << "Hello World!\n" <<15<< "\n">
---
+++ segundaprueba.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// using namespace std; // BAD
+
+int main(void) {std::cout << "hello world!\n" << 15 <<"\n";
+
+    return 0;
+}
